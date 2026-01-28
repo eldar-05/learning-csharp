b5dcf40a8e80e525ac0ae0fafbf91127fb602185
eldar-05 <eldarzh123@gmail.com>
2026-01-28 10:50:39 +0600

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // ###################  DATA TYPES USAGE #######################
             /*
             Console.WriteLine("Hello World!");
             int age = Console.Read();
@@ -16,12 +17,9 @@ namespace HelloWorld
             Console.WriteLine("Your age is: " + let + " and you have lived " + daysLived);
             */
 
-            byte a = 5;
-            for(int i = 0; i < 1; i++)
-            {
-                a++;
-                Console.WriteLine(a);
-            }
+
+            // ###################  BYTE DATA TYPE HOW ITS WORKING #######################
+
             /* output: it means it can store only 255 values, after that it will start from 0 again(((
              * 254
              * 255
@@ -31,9 +29,76 @@ namespace HelloWorld
              * 3
              */
 
-            //decimal
+            // ###################  LOGICAL OPERATIONS #######################
 
+            bool isCSharpFun = true;
+            bool amIGood = false;
+            bool isGodExist = true;
+            bool isEarthFlat = false;
+            // only one
+            Console.WriteLine(true | false);
+            Console.WriteLine(false | true);
+            Console.WriteLine(true | true);
+            Console.WriteLine(false | false);
+            Console.WriteLine();
+            // with ||
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || true);
+            Console.WriteLine(true || true);
+            Console.WriteLine(false || false);
+            Console.WriteLine("NO DIFFERENCE \n");
 
+            // ###################  IF  ELSE IN C# #######################
+            bool isSunny = true;
+            bool isRaining = false;
+            bool doingSports = true;
+            bool isAtHome = false;
+            if (isSunny == true && doingSports != false)
+            {
+                Console.WriteLine("I am happiest man in the world");
+            }
+            else if (isRaining == true && isAtHome == true)
+            {
+                Console.WriteLine("I am sad");
+            }
+            else if (isRaining == true && isAtHome == false || isRaining == true && doingSports == true)
+            {
+                Console.WriteLine("I am wet");
+            }
+            else
+            {
+                Console.WriteLine("I dont know what is going on here");
+            }
+
+            // ###################  TERNARY OPERATION  #######################
+
+            Console.WriteLine((isAtHome ? "I am sad" : "I am happy"));
+
+            // ###################  FOR LOOPS  #######################
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("The value of i is: " + i);
+            }
+
+            int j = 10;
+            while (j-- > 10)
+            {
+                Console.WriteLine(j + " hard");
+            }
+
+            j = 5;
+            do
+            {
+                Console.WriteLine("this code is running only if j > 10 but in do while loop it run a least once");
+                j++;
+            } while (j > 10);
+
+            int[] data = { 10, 20, 30, 40, 50 };
+            foreach (int part in data)
+            {
+                Console.WriteLine(part * 2);
+            }
         }
     }
 }
